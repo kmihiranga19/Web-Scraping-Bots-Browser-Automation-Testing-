f8ccb8ad844f274ffc6c019b6000af5167f0084f
Kavindu <kmvithanage48@gmail.com>
2024-04-23 19:43:34 +0530

    updates

--- booking/booking.py
+++ booking/booking.py
@@ -2,10 +2,22 @@ from selenium import webdriver
 import booking.constands as const
 
 
-class Booking(webdriver.Chrome):
-    def __init__(self, driver=webdriver.Chrome()):
+class Booking():
+    def __init__(self, teardown=False):  # we can handle __exit__ function with teardown boolean
+        self.teardown = teardown
+
+    def __enter__(self):
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option("detach", True)
+        driver = webdriver.Chrome(options=options)
         self.driver = driver
-        super(Booking, self).__init__()
+        self.driver.implicitly_wait(15)
+        self.driver.maximize_window()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):  # double underscore function run in the end, because of "WITH" in main file
+        if self.teardown:
+            self.driver.quit()
 
     def land_first_page(self):
-        self.get(const.BASE_URL)
+        self.driver.get(const.BASE_URL)
