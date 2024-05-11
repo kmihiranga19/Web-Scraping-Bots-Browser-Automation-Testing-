f627ff226b553bf70dbfb095646eec4f7f6b3cfb
Kavindu <kmvithanage48@gmail.com>
2024-05-11 20:51:04 +0530

    updates

--- booking/booking_filtration.py
+++ booking/booking_filtration.py
@@ -1,5 +1,6 @@
 import time
 
+from selenium.common import NoSuchElementException
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 
@@ -11,14 +12,18 @@ class BookingFiltration:
     def apply_star_rating(self):
         self.driver.execute_script("window.scrollTo(0, 2000)")
         time.sleep(3)
-        star_filtration_box = self.driver.find_element(By.ID, "filter_group_class_:r26:")
-        star_child_elements = star_filtration_box.find_elements(By.CSS_SELECTOR, "*")
+        try:
+            
 
-        for star_child_element in star_child_elements:
-            if star_child_element.get_attribute('data-filters-item') == "class:class=1":
-                star_child_element.click()
-                break
-            else:
-                print(star_child_element.get_attribute('innerHTML'))
+        except NoSuchElementException:
+            star_filtration_box = self.driver.find_element(By.ID, "filter_group_class_:r26:")
+            star_child_elements = star_filtration_box.find_elements(By.CSS_SELECTOR, "*")
+
+            for star_child_element in star_child_elements:
+                if star_child_element.get_attribute('data-filters-item') == "class:class=1":
+                    star_child_element.click()
+                    break
+                else:
+                    print(star_child_element.get_attribute('innerHTML'))
 
 
