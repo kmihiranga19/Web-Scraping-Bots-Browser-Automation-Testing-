5a0f7418890634cb80fdd9295cf938d9b4802a62
Kavindu <kmvithanage48@gmail.com>
2024-05-12 21:40:02 +0530

    updates

--- booking/booking_filtration.py
+++ booking/booking_filtration.py
@@ -9,7 +9,7 @@ class BookingFiltration:
     def __init__(self, driver: WebDriver):
         self.driver = driver
 
-    def apply_star_rating(self, star_value):
+    def apply_star_rating(self, *star_values):
         self.driver.execute_script("window.scrollTo(0, 2000)")
         time.sleep(3)
         try:
@@ -19,16 +19,17 @@ class BookingFiltration:
             star_filtration_box = self.driver.find_element(By.ID, "filter_group_class_:r26:")
             star_child_elements = star_filtration_box.find_elements(By.CSS_SELECTOR, "*")
 
-            for star_child_element in star_child_elements:
-                if star_child_element.get_attribute('data-filters-item') == f'class:class="{star_value}"':
-                    star_child_element.click()
-                    break
+            for star_value in star_values:
+                for star_child_element in star_child_elements:
+                    if star_child_element.get_attribute('data-filters-item') == f'class:class="{star_value}"':
+                        star_child_element.click()
+
 
         except NoSuchElementException:
             star_filtration_box = self.driver.find_element(By.ID, "filter_group_class_:r26:")
             star_child_elements = star_filtration_box.find_elements(By.CSS_SELECTOR, "*")
 
-            for star_child_element in star_child_elements:
-                if star_child_element.get_attribute('data-filters-item') == f'class:class="{star_value}"':
-                    star_child_element.click()
-                    break
+            for star_value in star_values:
+                for star_child_element in star_child_elements:
+                    if star_child_element.get_attribute('data-filters-item') == f'class:class="{star_value}"':
+                        star_child_element.click()
